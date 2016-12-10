d973934f5b08d931ca3643a863f51153564a73a1
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-12-10 12:21:38 -0500

    Update g_wav.h

--- g_wav.h
+++ g_wav.h
@@ -8,11 +8,11 @@ TMRpcm tmrpcm;
 char wave[] = "gwave.wav";
 
 void setup(){
-tmrpcm.speakerPin = 9;
-Serial.begin(9600);
-if (!SD.begin(SD_ChipSelectPin)) {
-Serial.println("SD fail");
-return;
+  tmrpcm.speakerPin = 9;
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin)) {
+   Serial.println("SD fail");
+exit error(code);
 }
 tmrpcm.setVolume(6); // max volume is 7
 tmrpcm.play(wave);
