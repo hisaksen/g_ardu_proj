18e8ff33daf34dcf88c203fbb4633d7258b8b714
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2016-12-12 19:10:24 -0500

    Update gwave.h

--- gwave.h
+++ gwave.h
@@ -8,13 +8,13 @@ TMRpcm tmrpcm;
 char wave[] = "gwave.wav";
 
 void setup(){
-  tmrpcm.speakerPin = 9;
-  Serial.begin(9600);
-  if (!SD.begin(SD_ChipSelectPin)) {
-   Serial.println("SD fail");
-return
+tmrpcm.speakerPin = 9;
+Serial.begin(9600);
+if (!SD.begin(SD_ChipSelectPin)) {
+Serial.println("SD fail");
+return;
 }
-tmrpcm.setVolume(6); // max volume is 7
+tmrpcm.setVolume(6);
 tmrpcm.play(wave);
 }
 
