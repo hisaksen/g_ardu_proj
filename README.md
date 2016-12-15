280f292eeb6197fabf59c72add398cb0dc83a0ae
Dottin-adm <garrethdottin@gmail.com>
2016-12-15 14:07:10 -0500

    touch ups to music_player

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # g_ardu_proj
+# Goal of Project: Creating a small arduino music player capable of sensing when a person passes by.
+# Phase 1
+# Music player that can play a song on a loop when it senses
+# Hardware
+    Arduino Uno, 8 ohms Speaker , PIR Sensor9 voltz battery
+
+#Phase 2
+    Shrunken down version of the same application with an arduino nano and two speakers
--- music_player.h
+++ music_player.h
@@ -4,7 +4,9 @@
 #include "SPI.h"
 
 TMRpcm audio;
- 
+
+char wave[] = "gwave.wav";
+
 int ledPin = 13;                // Using Arduino Uno's built in LED to test sensor
 int inputPin = 2;               // PIR sensor PIN
 int pirState = LOW;             // Motion detection starts at off
@@ -13,6 +15,7 @@ int val = 0;                    // Instantiate PIN reading
 void setup() {
   pinMode(ledPin, OUTPUT);      // LED as Output
   pinMode(inputPin, INPUT);     // Sensor as Input
+  audio.speakerPin = 10; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
   audio.setVolume(6);
   Serial.begin(9600);
 }
@@ -23,6 +26,5 @@ void loop(){
       // Check to see if PIR picks up on motion and song is not playing
       Serial.println("Motion detected!");
       audio.play(wave);
-  }
   }
 }
