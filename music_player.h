5ce6bb61f62c92d720063e13de53c14ceb79bb6d
GarrethDottin <garrethdottin@gmail.com>
2016-12-21 16:09:30 -0500

    refactor music player to work with uno

--- music_player.h
+++ music_player.h
@@ -7,22 +7,28 @@ TMRpcm audio;
 
 char wave[] = "gwave.wav";
 
-int ledPin = 13;                // Using Arduino Uno's built in LED to test sensor
 int inputPin = 2;               // PIR sensor PIN
 int pirState = LOW;             // Motion detection starts at off
 int val = 0;                    // Instantiate PIN reading
  
 void setup() {
-  pinMode(ledPin, OUTPUT);      // LED as Output
-  pinMode(inputPin, INPUT);     // Sensor as Input
   audio.speakerPin = 10; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  pinMode(inputPin, INPUT);     // Sensor as Input
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD fail");
+    return;
+  }
   audio.setVolume(6);
   Serial.begin(9600);
+  
 }
 
 void loop(){
   val = digitalRead(inputPin);
-  if (val == HIGH && audio.isPlaying() == 0) {
+  int personDetected = 1;
+  int audioInProgress = 0;
+  Serial.println(val);
+  if (val == personDetected && audio.isPlaying() == audioInProgress) {
       // Check to see if PIR picks up on motion and song is not playing
       Serial.println("Motion detected!");
       audio.play(wave);
