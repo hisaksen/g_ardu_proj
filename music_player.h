5929e6246e0dbfac790da59934c03e24fd53e158
GarrethDottin <garrethdottin@gmail.com>
2016-12-21 16:34:26 -0500

    music_player working

--- music_player.h
+++ music_player.h
@@ -9,8 +9,11 @@ char wave[] = "gwave.wav";
 
 int inputPin = 2;               // PIR sensor PIN
 int pirState = LOW;             // Motion detection starts at off
-int val = 0;                    // Instantiate PIN reading
- 
+int val = 0;
+int delayReading = 0; 
+int personDetected = 1;
+int audioInProgress = 0;
+
 void setup() {
   audio.speakerPin = 10; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
   pinMode(inputPin, INPUT);     // Sensor as Input
@@ -24,13 +27,22 @@ void setup() {
 }
 
 void loop(){
+  Serial.println("Before wait");
+  delay(delayReading);   
+  Serial.println("After delayReading interval");              
+  
   val = digitalRead(inputPin);
-  int personDetected = 1;
-  int audioInProgress = 0;
+  
   Serial.println(val);
   if (val == personDetected && audio.isPlaying() == audioInProgress) {
+      
+      delayReading = 10000;  // The length of the song 
       // Check to see if PIR picks up on motion and song is not playing
       Serial.println("Motion detected!");
       audio.play(wave);
+  }
+  else { 
+      delayReading = 0; 
+  }
   }
 }
